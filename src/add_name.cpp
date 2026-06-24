24be068fb74d54058ccda1388a334a54fbe4cbc4
megakev10 <kevmenz6022103@gmail.com>
2026-06-24 08:27:44 +0100

    1st update tne add name

--- src/add_name.cpp
+++ src/add_name.cpp
@@ -1,25 +1,50 @@
 #include <Arduino.h>
+#include <Preferences.h>
+
+Preferences pref;
+int d;
 
 class identification{
     public:
     String take_name();
+    void Saving();
     private:
      String name;
 };
 
 String identification::take_name(){
-    Serial.println("Veuillez entrer votre nom");
-    if(Serial.available()){
-        name = Serial.readString();
-        return name;
-    }   
+    while (!Serial.available()){}
+    name = Serial.readString();
+    return name;     
+}
+
+void identification::Saving(){
+    pref.begin("users_infos", false);
+        pref.putString("names", take_name());
+        Serial.printf("salut %s", pref.getString("name"));
+    pref.end();
+}
+
+int decision(){
+    Serial.println("Entrez 1 pour lire le nom enregistre, 2 pour mettre votre nom");
+    while (!Serial.available()){ }
+    d = Serial.parseInt();
+    return d;
 }
 
 void setup(){
+    Serial.begin(115200);
     identification identity;
+    d = decision();
+    if(d == 1){
+        identity.Saving();
+    }else{
     Serial.println("lancons le test d'identification");
+    Serial.println("Veuillez entrer votre nom");
+    delay(4000);
     String n = identity.take_name();
     Serial.println(n);
-}
+    identity.Saving();
+}}
 
 void loop(){}
